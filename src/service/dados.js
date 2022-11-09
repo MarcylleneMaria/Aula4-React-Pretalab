16edadfc593eb70d80c9c1a737dfbb95da2deb63
MarcylleneMaria <marcyllenemaria@id.uff.br>
2022-11-08 21:25:09 -0300

    criando listas e chaves

---
+++ src/service/dados.js
@@ -0,0 +1,40 @@
+const filmes = [
+    {
+        id:1,
+        nome:"Procurando Nemo",
+        genero:"Animation, Adventure, Comedy",
+        capa:"https://th.bing.com/th/id/R.38b7de0abe790b83518e2bf0c764f1c6?rik=bj6Id8IDgRXZ3g&riu=http%3a%2f%2f1.bp.blogspot.com%2f-ztTUKvO6_8s%2fT-mhpcXaZbI%2fAAAAAAAAHsI%2fYhNRLVaws2k%2fs1600%2fFinding%2bNemo%2b(2003)%2b4.jpg&ehk=xXeBuVIbEg6yMk66EzY4u2iqtAKkjbem25Ywc06lERM%3d&risl=&pid=ImgRaw&r=0",
+    },
+    {
+        id:2,
+        nome:"",
+        genero:"",
+        capa:"",
+    },
+    {
+        id:3,
+        nome:"",
+        genero:"",
+        capa:"",
+    },
+    {
+        id:4,
+        nome:"",
+        genero:"",
+        capa:"",
+    },
+    {
+        id:5,
+        nome:"",
+        genero:"",
+        capa:"",
+    },
+    {
+        id:6,
+        nome:"",
+        genero:"",
+        capa:"",
+    },
+]
+
+export default filmes;
